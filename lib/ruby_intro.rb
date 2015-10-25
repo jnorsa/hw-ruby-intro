168a8091c047151bb276eceb4ddf0e7f3ffd4567
Johann Norsa <johann.norsa.pub@gmail.com>
2015-10-25 13:04:57 +0000

    part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,42 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  summtotal = 0
+  if arr.count == 0 then
+    return 0
+  else
+    arr.each {|x| summtotal+=x}
+    return summtotal
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.count == 0 then
+    return 0
+  elsif arr.count == 1 then
+    arr_temp = arr.sort
+    arr_temp.reverse!
+    return arr_temp[0]
+  else
+    arr_temp = arr.sort
+    arr_temp.reverse!
+    return arr_temp[0]+arr_temp[1]    
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.count == 0 then
+    return false
+  elsif arr.count == 1 then
+    return false
+  else
+    for i in 0..(arr.count-1)
+      for j in (i+1)..(arr.count-1)
+        if (arr[i]+arr[j] == n) then return true end
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
