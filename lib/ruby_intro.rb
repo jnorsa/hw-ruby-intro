ee6c04030507663702ba1b86f7296204b98c6828
Johann Norsa <johann.norsa.pub@gmail.com>
2015-10-25 14:01:27 +0000

    HW 0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -44,19 +44,32 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s == '' then return false end
+  return (s =~ /^[^AEIOU\W](.*)+/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if !(s =~ /^(0|1)+\z/) then return false end
+  num = s.to_i(2)
+  return (num % 4 == 0)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize(isbn , price)
+    if (isbn == "" || price<=0) then raise ArgumentError end
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    return "$" + ('%.2f' % price)
+  end
 end
